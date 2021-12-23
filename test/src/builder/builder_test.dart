f306c3574d9a9f86d8bb449a02c061dbfe64526d
Brett Sutton <bsutton@noojee.com.au>
2021-12-24 10:00:45 +1100

    Added test of build.

---
+++ test/src/builder/builder_test.dart
@@ -0,0 +1,21 @@
+import 'package:dcli/dcli.dart';
+import 'package:simple_mysql_orm/simple_mysql_orm.dart';
+import 'package:test/test.dart';
+
+import '../../test_dao/dao/publisher_dao.dart';
+
+final settingsPath = join('test', 'settings.yaml');
+void main() {
+  setUp(() {
+    if (!exists(dirname(settingsPath))) {
+      createDir(dirname(settingsPath), recursive: true);
+    }
+    DbPool.fromSettings(pathToSettings: settingsPath);
+  });
+
+  test('builder ...', () async {
+    await withTransaction<void>(() async {
+      await PublisherDao().delete().where().eq('name', 'brett').run();
+    });
+  });
+}
