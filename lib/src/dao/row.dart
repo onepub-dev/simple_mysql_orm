2cce8cae170058ed75cacfee95644c403ba6f3fe
Brett Sutton <bsutton@noojee.com.au>
2021-12-24 09:59:30 +1100

    Added support for json encoding maps when saving a field to db.

--- lib/src/dao/row.dart
+++ lib/src/dao/row.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
+
 class Row {
   Row(this.fields);
   Map<String, dynamic> fields;
 
-  String fieldAsString(String name) => fields[name] as String;
+  String fieldAsString(String name) => fields[name].toString();
 
   int fieldAsInt(String name) => fields[name] as int;
+
+  /// Currently expects a Blob
+  Map<String, dynamic> fieldAsMap(String name) =>
+      jsonDecode(fields[name].toString()) as Map<String, dynamic>;
 
   bool fieldAsBool(String name) => (fields[name] as int) == 1;
   DateTime fieldAsDateTime(String name) => fields[name] as DateTime;
