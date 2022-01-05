8a16aadb935d2ef70737d20b2d10442cc941cc25
Brett Sutton <bsutton@noojee.com.au>
2022-01-05 22:12:17 +1100

    Added addtional field conversions.

--- lib/src/dao/row.dart
+++ lib/src/dao/row.dart
@@ -1,10 +1,14 @@
 import 'dart:convert';
 
+import 'package:date_time/date_time.dart';
+import 'package:money2/money2.dart';
+
 class Row {
   Row(this.fields);
   Map<String, dynamic> fields;
 
   String fieldAsString(String name) => fields[name].toString();
+
   String? fieldAsStringNullable(String name) => fields[name]?.toString();
 
   int fieldAsInt(String name) => fields[name] as int;
@@ -15,5 +19,19 @@ class Row {
 
   bool fieldAsBool(String name) => (fields[name] as int) == 1;
   DateTime fieldAsDateTime(String name) => fields[name] as DateTime;
-  DateTime fieldAsDate(String name) => fields[name] as DateTime;
+  Date fieldAsDate(String name) => Date.from(fields[name] as DateTime);
+
+  /// Extract the field [name] as a nullable Date
+  Date? fieldAsDateNullable(String name) {
+    if (fields[name] == null) {
+      return null;
+    }
+    return Date.from(fields[name] as DateTime);
+  }
+
+  Time fieldAsTime(String name) => Time.fromStr(fields[name] as String)!;
+  Money fieldAsMoney(String name, Currency currency) =>
+      Money.fromIntWithCurrency(fields[name] as int, currency);
 }
+
+// DateTime parse(String) => 
