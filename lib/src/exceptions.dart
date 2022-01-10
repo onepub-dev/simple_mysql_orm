19f53f77bf23aca15322cb544e8a56f25982e7e5
Brett Sutton <bsutton@noojee.com.au>
2022-01-11 07:30:36 +1100

    more exceptions.

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -1,21 +1,13 @@
-class TooManyResultsException extends MySqlORMException {
-  TooManyResultsException(String message) : super(message);
-}
-
-class UnknownEntityIdException extends MySqlORMException {
-  UnknownEntityIdException(String message) : super(message);
+class ConfigurationException extends MySqlORMException {
+  ConfigurationException(String message) : super(message);
 }
 
 class DatabaseIntegrityException extends MySqlORMException {
   DatabaseIntegrityException(String message) : super(message);
 }
 
-class NestedTransactionException extends MySqlORMException {
-  NestedTransactionException(String message) : super(message);
-}
-
-class ConfigurationException extends MySqlORMException {
-  ConfigurationException(String message) : super(message);
+class IdentityNotSetException extends MySqlORMException {
+  IdentityNotSetException(String message) : super(message);
 }
 
 class MissingTenantException extends MySqlORMException {
@@ -23,9 +15,21 @@ class MissingTenantException extends MySqlORMException {
 }
 
 class MySqlORMException implements Exception {
-  MySqlORMException(this.message);
   String message;
+  MySqlORMException(this.message);
 
   @override
   String toString() => message;
+}
+
+class NestedTransactionException extends MySqlORMException {
+  NestedTransactionException(String message) : super(message);
+}
+
+class TooManyResultsException extends MySqlORMException {
+  TooManyResultsException(String message) : super(message);
+}
+
+class UnknownEntityIdException extends MySqlORMException {
+  UnknownEntityIdException(String message) : super(message);
 }
