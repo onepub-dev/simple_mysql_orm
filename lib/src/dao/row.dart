fc5af9b22a25fc68738bb6363469042f51530a30
Brett Sutton <bsutton@noojee.com.au>
2022-01-16 14:21:45 +1100

    BREAKING: changed all tryAs... methods to try... for consistency.

--- lib/src/dao/row.dart
+++ lib/src/dao/row.dart
@@ -9,11 +9,11 @@ class Row {
 
   /// convert the field with [name] to a string.
   String asString(String name) => fields[name].toString();
-  String? tryAsString(String name) => fields[name]?.toString();
+  String? tryString(String name) => fields[name]?.toString();
 
   /// convert the field with [name] to a int.
   int asInt(String name) => fields[name] as int;
-  int? tryAsInt(String name) =>
+  int? tryInt(String name) =>
       fields[name] == null ? null : fields[name] as int;
 
   /// convert the field with [name] to a Map.
@@ -23,7 +23,7 @@ class Row {
 
   /// convert the field with [name] to a bool.
   bool asBool(String name) => (fields[name] as int) == 1;
-  bool? tryAsBool(String name) =>
+  bool? tryBool(String name) =>
       (fields[name] == null ? null : fields[name] as int) == 1;
 
   /// convert the field with [name] to a DateTime.
@@ -33,18 +33,18 @@ class Row {
 
   /// convert the field with [name] to a Date.
   Date asDate(String name) => Date.from(fields[name] as DateTime);
-  Date? tryAsDate(String name) =>
+  Date? tryDate(String name) =>
       fields[name] == null ? null : fields[name] as Date;
 
   /// convert the field with [name] to a Date.
   Time asTime(String name) => Time.fromStr(fields[name] as String)!;
-  Time? tryAsTime(String name) =>
+  Time? tryTime(String name) =>
       fields[name] == null ? null : fields[name] as Time;
 
   Money asMoney(String name, Currency currency) =>
       Money.fromIntWithCurrency(fields[name] as int, currency);
 
-  Money? tryAsMoney(String name) =>
+  Money? tryMoney(String name) =>
       fields[name] == null ? null : fields[name] as Money;
 
   T? asCustom<T>(String name, T? Function(Object? value) convertTo) {
